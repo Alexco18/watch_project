e40c0b6514993f26dc0028f197191a40d4781423
Alexandre <coralexandre36@gmail.com>
2026-02-10 14:45:58 +0100

    Add Rolex watches to the WATCHES array

---
+++ brands/rolex.js
@@ -0,0 +1,18 @@
+window.WATCHES.push(
+  {
+    brand: "Rolex",
+    id: "rolex-daytona",
+    name: "Daytona",
+    year: 2016,
+    img: "https://via.placeholder.com/300",
+    lastUpdate: "2026-02-10"
+  },
+  {
+    brand: "Rolex",
+    id: "rolex-submariner",
+    name: "Submariner",
+    year: 1953,
+    img: "https://via.placeholder.com/300",
+    lastUpdate: "2026-02-10"
+  }
+);
