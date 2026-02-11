1998e518f74606ab65d5412bda71eb70330e416e
Alexandre <coralexandre36@gmail.com>
2026-02-11 16:21:12 +0100

    Update swatch.js

--- brands/swatch.js
+++ brands/swatch.js
@@ -3,7 +3,7 @@ window.WATCHES.push({
   id: "moonswatch-mission-to-the-moon",
   name: "MoonSwatch – Mission to the Moon",
   year: 2022,
-  img: "liens-photos/swatch-pic/moonswatch-1.jpg",
+  img: "liens-photos/swatch-pic/moonswatch_bleue_pic.avif",
   description: "Une réinterprétation ludique et accessible de la Speedmaster Moonwatch.",
   specs: [
     { label: "Boîtier", value: "Bioceramic 42 mm" },
