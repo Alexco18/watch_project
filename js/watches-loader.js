133dfc89e4bc2babf484deeb40935da5f07fedbf
Alexandre <coralexandre36@gmail.com>
2026-02-11 09:34:31 +0100

    Fix import paths and formatting in loadAllBrands

--- js/watches-loader.js
+++ js/watches-loader.js
@@ -1,15 +1,17 @@
-async function loadAllBrands(){
+async function loadAllBrands() {
   const brandFiles = [
-    "brands/rolex.js",
-    "brands/omega.js",
-    "brands/lip.js"
+    "/brands/rolex.js",
+    "/brands/omega.js",
+    "/brands/lip.js"
   ];
 
   window.WATCHES = [];
 
-  for(const file of brandFiles){
-    await import(`../${file}`);
+  for (const file of brandFiles) {
+    await import(file);
   }
 
   return window.WATCHES;
 }
+
+export { loadAllBrands };
