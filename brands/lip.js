f35607eef5d1618775f04ae44a43fcb88dc0b4ff
Alexandre <coralexandre36@gmail.com>
2026-02-11 13:11:53 +0100

    Update lip.js

--- brands/lip.js
+++ brands/lip.js
@@ -19,4 +19,15 @@ window.WATCHES.push({
   specs: [
     { label: "Boîtier", value: "Acier 38 mm" }
   ]
+},
+                            {
+  brand: "Lip",
+  id: "lip-incabloc",
+  name: "Incabloc",
+  year: 1963,
+  img: "https://via.placeholder.com/300",
+  description: "La simplisité sur un poignet",
+  specs: [
+    { label: "Boîtier", value: "Acier 40 mm" }
+  ]
 });
