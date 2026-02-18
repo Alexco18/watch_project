17aa9e3caf25b4f2952d2bca8b91fa1a6e12ace2
Alexandre <coralexandre36@gmail.com>
2026-02-18 12:15:33 +0100

    Create breitling.js

---
+++ brands/breitling.js
@@ -0,0 +1,11 @@
+window.WATCHES.push({
+  brand: "Breitling",
+  id: "breitling-test",
+  name: "Test",
+  year: 1946,
+  img: "https://via.placeholder.com/300",
+  description: "Un modèle historique.",
+  specs: [
+    { label: "Boîtier", value: "Acier 38 mm" }
+  ]
+});
