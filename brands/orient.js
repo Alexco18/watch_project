ffe6c06fca8c26fdc3ebfd96dc163669a9303b94
Alexandre <coralexandre36@gmail.com>
2026-02-18 12:10:07 +0100

    Create orient.js

---
+++ brands/orient.js
@@ -0,0 +1,11 @@
+window.WATCHES.push({
+  brand: "Orient",
+  id: "orient-test",
+  name: "TEST",
+  year: 1948,
+  img: "https://via.placeholder.com/300",
+  description: "Un modèle historique.",
+  specs: [
+    { label: "Boîtier", value: "Acier 38 mm" }
+  ]
+});
