2d6a3593ca783c2ad92c4b1af98dd89c63535aa9
Alexandre <coralexandre36@gmail.com>
2026-02-18 12:06:57 +0100

    Create yema.js

---
+++ brands/yema.js
@@ -0,0 +1,11 @@
+window.WATCHES.push({
+  brand: "yema",
+  id: "yema-test",
+  name: "TEST",
+  year: 1951,
+  img: "https://via.placeholder.com/300",
+  description: "Un modèle historique.",
+  specs: [
+    { label: "Boîtier", value: "Acier 38 mm" }
+  ]
+});
