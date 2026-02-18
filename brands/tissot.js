af87d126b19aa4b5d2adf2877a410c1b6a84620b
Alexandre <coralexandre36@gmail.com>
2026-02-18 12:08:33 +0100

    Create tissot.js

---
+++ brands/tissot.js
@@ -0,0 +1,11 @@
+window.WATCHES.push({
+  brand: "Tissot",
+  id: "tissot-test",
+  name: "TEST",
+  year: 1949,
+  img: "https://via.placeholder.com/300",
+  description: "Un modèle historique.",
+  specs: [
+    { label: "Boîtier", value: "Acier 38 mm" }
+  ]
+});
